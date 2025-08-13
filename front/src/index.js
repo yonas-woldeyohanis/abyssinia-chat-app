eff25860c100b37edae5fa42c4789fb8f6b5c52a
Yonas <yonaswjohn@gmail.com>
2025-08-13 11:26:38 +0300

    this

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,11 @@
  // client/src/index.js
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
